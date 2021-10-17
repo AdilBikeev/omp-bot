3785296a1e5e040c1b6d57b0f30c3bab3ec7b768
AdilBikeev <bikeev1999@mail.ru>
2021-10-18 00:53:57 +0300

    feature/task-1 Add check exist .env

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -10,7 +10,10 @@ import (
 )
 
 func main() {
-	_ = godotenv.Load()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	token, found := os.LookupEnv("TOKEN")
 	if !found {
